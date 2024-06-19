964dffb8a53ce84927eab2ea98a46950d4a3b07f
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2024-06-19 12:06:31 +0200

    feat: `ContainerConfig.postgres()` helper function (#14)

--- src/main/kotlin/com/optravis/jooq/gradle/Config.kt
+++ src/main/kotlin/com/optravis/jooq/gradle/Config.kt
@@ -10,6 +10,12 @@ import java.io.Serializable
 )
 public annotation class ExperimentalJooqGeneratorConfig
 
+private object Default {
+    const val DB = "jooq"
+    const val USER = "jooq"
+    const val PASSWORD = "jooq"
+}
+
 @OptIn(ExperimentalJooqGeneratorConfig::class)
 internal data class JooqRootConfig(
     val container: ContainerConfig,
@@ -33,10 +39,16 @@ public data class ContainerConfig(
     internal val port: Int,
     internal val environment: Map<String, String>
 ) : Serializable {
-    internal companion object {
-        fun postgres(db: String, user: String, password: String, version: Int = 16) = ContainerConfig(
+    public companion object {
+        public fun postgres(
+            db: String = Default.DB,
+            user: String = Default.USER,
+            password: String = Default.PASSWORD,
+            version: String = "16",
+            port: Int = 5432,
+        ): ContainerConfig = ContainerConfig(
             image = "postgres:$version",
-            port = 5432,
+            port = port,
             environment = mapOf(
                 "POSTGRES_DB" to db,
                 "POSTGRES_USER" to user,
@@ -77,8 +89,12 @@ public data class DbConnectionConfig(
     /** JDBC url template. `{{port}}` will be substituted with the container port */
     internal val urlTemplate: String,
 ) : Serializable {
-    internal companion object {
-        fun postgres(db: String, user: String, password: String) = DbConnectionConfig(
+    public companion object {
+        public fun postgres(
+            db: String = Default.DB,
+            user: String = Default.USER,
+            password: String = Default.PASSWORD
+        ): DbConnectionConfig = DbConnectionConfig(
             user = user,
             password = password,
             urlTemplate = "jdbc:postgresql://localhost:{{port}}/$db"
